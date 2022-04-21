cb72eb75844eaab96a47b6f72de90c37c8ad1290
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 19:50:37 +0700

    add validation for invalid price on order_menu spec

--- spec/models/order_menu_spec.rb
+++ spec/models/order_menu_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe OrderMenu, type: :model do
+  customer = FactoryBot.create(:customer)
+  order = FactoryBot.create(:order, customer_id: customer.id)
   it "is valid with order_id, price, name, and qty" do
-    customer = FactoryBot.create(:customer)
-    order = FactoryBot.create(:order, customer_id: customer.id)
     order_menu = FactoryBot.create(:order_menu, order_id: order.id)
     expect(order_menu).to be_valid
+  end
+  it "is invalid with price which have non numeric values" do
+    order_menu = FactoryBot.build(:order_menu, order_id: order.id, price: "haha")
+    expect(order_menu).to_not be_valid
+  end
+  it "is invalid with price less than 0.01" do
+    order_menu = FactoryBot.build(:order_menu, order_id: order.id, price: 0.001)
+    expect(order_menu).to_not be_valid
   end
 end
