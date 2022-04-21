2d2c99dc7ba1687afe54d12e5387f8ca9840e21e
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 17:54:15 +0700

    add uniqueness to customer email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
     validates :name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness :true
     validates_format_of :email, :with => /\A(\S+)@(.+)\.(\S+)\z/i
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
+    belongs_to :customer
     validates :status, presence: true,  inclusion: { in: %w(NEW PAID CANCELLED), message: "%{value} not valid value" }
 end
