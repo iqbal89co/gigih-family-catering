64bf854032c6d41b5ec731dd848a36624a2f7aac
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 21:41:27 +0700

    post menus/create routes controller spec

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -14,4 +14,30 @@ class MenusController < ApplicationController
     @menu = Menu.get_menu(params[:id])
     @menu
   end
+  def create
+    @menu = Menu.new(menu_params)
+    respond_to do |format|
+      if @menu.save
+        format.html {
+          redirect_to menu_url(@menu), notice: "Menus was successfully created."
+        }
+        format.json {
+          render :show, status: :created, location: @menu
+        }
+      else
+        format.html {
+          render :new, status: :unprocessable_entity
+        }
+        format.json {
+          render json: @menu.errors, status: :unprocessable_entity
+        }
+      end
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, :price)
+  end
 end
