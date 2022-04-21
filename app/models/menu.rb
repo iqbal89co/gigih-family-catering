bb15bf4f7ce98eb157d898ac9495120c44b6e057
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 20:18:03 +0700

    add get and get_all on menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,4 +4,14 @@ class Menu < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true, length: {maximum: 150}
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01}
+
+    def self.get_all_menus
+        @menus = Menu.all
+        @menus
+    end
+
+    def self.get_menu(id)
+        @menu = Menu.find_by(id: id)
+        @menu
+    end
 end
