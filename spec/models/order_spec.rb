bec95a8028de1fd3220e1b959637caabf77d990a
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 17:47:30 +0700

    add validate valid status to order spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with status" do
+    order = FactoryBot.create(:order)
+    expect(order).to be_valid
+  end
 end
