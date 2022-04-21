9f6a0b4951221b2dd54d909f3f39fef389d2d6cd
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 18:26:44 +0700

    add validation for order_menu model

--- app/models/order_menu.rb
+++ app/models/order_menu.rb
@@ -1,3 +1,12 @@
 class OrderMenu < ApplicationRecord
     belongs_to :order
+    validates :name, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01}
+    validates :qty, presence: true, numericality: { only_integer: true}
+
+    validate :menu_exists
+
+    def menu_exists
+        errors.add(:base, "Menu doesn't exist in database") unless Menu.name.include?(self.name)
+    end
 end
