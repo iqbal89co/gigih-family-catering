bc294e63b4a63818f1df8819eebaf7ca7551c487
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 17:23:01 +0700

    add validate valid email format to customer spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Customer, type: :model do
     customer = FactoryBot.create(:customer)
     expect(customer).to be_valid
   end
+  it "is valid email format" do
+    customer = FactoryBot.create(:customer)
+    expect(customer).to be_valid
+  end
   it "is invalid with wrong email format" do
     customer = FactoryBot.create(:invalid_email_customer)
     expect(customer).to_not be_valid
