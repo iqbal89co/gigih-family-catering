0e6855e3905000bc16be1ba12ea94a42fefa7dff
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 16:45:55 +0700

    add validate invalid name with duplicates to category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Category, type: :model do
 
     expect(category).to be_valid
   end
+  it "is invalid with duplicate name" do
+    category1 = FactoryBot.create(:category, name: "indian cuisine")
+    category2 = FactoryBot.build(:category, name: "indian cuisine")
+
+    category2.valid?
+    expect(category2.errors[:name]).to include("has already been taken")
+  end
 end
