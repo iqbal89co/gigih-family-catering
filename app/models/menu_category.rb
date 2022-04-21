5d37c60f07f275b0009c62d6d7689fb6c41eedbc
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 15:57:38 +0700

    update MenuCategory Validation

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,6 +1,6 @@
 class MenuCategory < ApplicationRecord
     belongs_to :menu
     belongs_to :category
-    validates :menu_id, presence: true
-    validates :category_id, presence: true
+    validates :menu_id, presence: true, numericality: { only_integer: true}
+    validates :category_id, presence: true, numericality: { only_integer: true}
 end
