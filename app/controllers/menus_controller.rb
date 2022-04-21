5a5d24d855340c6b96aee51fc44a0cdfc55de1ee
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 20:58:12 +0700

    index routes controller spec

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,10 @@
 class MenusController < ApplicationController
   def index
+    @menus = Menu.get_all_menus
+    @menus
   end
+  # def show
+  #   @menu = Menu.get_menu(params[:id])
+  #   @menu
+  # end
 end
--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe FoodsController do
+RSpec.describe MenusController do
+    describe 'GET#index' do
+        context 'all menus' do
+            it "populates an array of all menus" do
+                menu1 = create(:menu)
+                menu2 = create(:menu)
+                get :index
+                expect(assigns(:menus)).to match_array([menu1, menu2])
+            end
+            it "renders the :index template" do
+                get :index
+                expect(response).to render_template :index
+            end
+        end
+    end
 end
