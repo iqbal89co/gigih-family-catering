ea255e4e3c8a47c2375470fa2426ec124c1f9e19
iqbal89co <iqbalfakhriza089@gmail.com>
2022-04-21 15:47:18 +0700

    make menu validation

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,7 @@
 class Menu < ApplicationRecord
     has_many :menu_category
     has_many :category, through: :menu_category
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true, length: {maximum: 150}
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01}
 end
